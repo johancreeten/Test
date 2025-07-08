9a154cbcec3e6ff768864e97e0945022a8a2ec7d
Johan Creeten <johan.creeten@gmail.com>
2025-07-08 20:57:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -600,13 +600,12 @@ function showResult() {
   }, 0);
 
   // Attribution du profil selon la plage de points
-let profil = profils.find(p => {
-  let profil = null;
-  if (score <= 3) profil = profils[0];
-  else if (score <= 6) profil = profils[1];
-  else if (score <= 10) profil = profils[2];
-  else if (score <= 13) profil = profils[3];
-  else profil = profils[4];
+let profil;
+if (score <= 3) profil = profils[0];
+else if (score <= 6) profil = profils[1];
+else if (score <= 10) profil = profils[2];
+else if (score <= 13) profil = profils[3];
+else profil = profils[4];
 });
 
   // Calcul du pourcentage sur 16

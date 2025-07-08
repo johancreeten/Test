05465b98aa6800d4928d2ed72c84b208dfa93cbf
Johan Creeten <johan.creeten@gmail.com>
2025-07-08 21:01:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -606,7 +606,6 @@ else if (score <= 6) profil = profils[1];
 else if (score <= 10) profil = profils[2];
 else if (score <= 13) profil = profils[3];
 else profil = profils[4];
-});
 
   // Calcul du pourcentage sur 16
   const maxScore = 16;
